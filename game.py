e0e2b027d03eb76eca1c45aa210f4a26e2b02ae3
m26unknw <shukla9915294376@gmail.com>
2020-12-30 18:11:00 +0530

    added bunch of func

--- game.py
+++ game.py
@@ -3,6 +3,13 @@ board=["_", "_", "_",
        "_", "_", "_",
        "_", "_", "_"]
 
+game_on=True
+
+current_player= "X"
+
+
+winner= None
+
 def display_board():
      print(board[0] + " | " + board[1] + " | " + board[2])
      print(board[3] + " | " + board[4] + " | " + board[5])
@@ -14,11 +21,50 @@ def play_game():
     #show intial board
     display_board()
 
-    handle_turn()
+    while game_on:
 
 
-def handle_turn():
-    position=input("Enter any number in range 1-9:")
+        handle_turn(current_player)
+
+        check_if_game_over()
+
+        flip_player()
+
+    if winner=="X" or winner=="O":
+        print(winner + "Won.")
+    elif winner==None:
+        print("Tie.")
+
+
+
+def handle_turn(player):
+    position=int(input("Enter any number in range 1-9: ")) #Here asking for user to which postion he want to put his turn
+    position= position-1 # Updating the value with index value
     
+    board[position]=str(input("Enter X or O: ")).upper()
+    display_board()
+ 
+def check_if_game_over():
+
+    check_if_win()
+
+    check_if_tie()
+
+def check_if_win():
+    
+    return 
+
+
+def check_if_tie():
+
+    return
+
+def flip_player():
+    return
+
+
 
 play_game()
+
+
+
